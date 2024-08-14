056a787ca783ed3c0031c46967bc2901f96c74bb
Pst4r8 <94172938+pst4r8@users.noreply.github.com>
2024-08-14 08:16:16 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
     name: 'quote',
     script: './index.js',
     max_memory_restart: '1000M',
-    instances: 1,
-    exec_mode: 'cluster',
+    //instances: 1,
+    //exec_mode: 'cluster',
     watch: true,
     ignore_watch: ['node_modules', 'assets'],
     env: {
